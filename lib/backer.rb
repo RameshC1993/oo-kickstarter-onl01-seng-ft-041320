a01b41144022d87cc47deb5a4058808371c90a4a
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-05-11 20:43:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,8 @@
 class Backer 
-  attr_accessor :name, :backed_project
+  attr_accessor :name, :backed_projects
   
   def initialize(name)
     @name = name 
-    @backed_project = []
+    @backed_projects = []
   end
 end
